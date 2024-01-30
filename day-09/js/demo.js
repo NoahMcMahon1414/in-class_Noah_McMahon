914b55fb218bb0b14feb0d1b157218f112375446
Noah McMahon <nam1414@uw.edu>
2024-01-29 17:20:15 -0800

    Finished Day 9

--- day-09/js/demo.js
+++ day-09/js/demo.js
@@ -3,8 +3,7 @@
 // use querySelector and css selectors to get the none.css href
 // log it
 
-
-
+let h1 = document.querySelector(h1)
 
 //select h1 and modify it's font-size
 
@@ -15,13 +14,32 @@
 //log them
 //log the target
 
+let button = document.querySelector('button')
+button.addEventListener('click', function(event) {
+    
+let message = "You clicked on " + event.offSetX + " and " + event.offSetY;
 
+let clickedElement = event.target;
+clickedElement.style.fontSize = "2em";
 
+console.log(message)
+console.log(clickedElement)
+
+});
 
 //key press event listener
 
+document.addEventListener('keydown', function(event) {
 
+    if(event.key == "ArrowUp")
+    {
+        console.log('Going Up')
+    } else if (event.key == "ArrowDwn")
+    {
+        console.log("Arrow Down!")
+    } 
 
+});
 
 //get all style sheet links, log them, then add even listeners to each
 

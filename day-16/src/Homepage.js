af342b657d30f8587745e4b4f129ccb8732dd464
Noah McMahon <nam1414@uw.edu>
2024-02-26 17:30:34 -0800

    Partially Done with Day 16

---
+++ day-16/src/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Homepage()
+{
+    return (
+        <div className='container'>
+            <h1>Homepage of Noah McMahon</h1>
+        </div>
+    );
+}
